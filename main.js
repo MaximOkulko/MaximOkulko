1dc6cb5c6d946e904ac5b128f9fc564e10af4b9b
maxim <maxim_okulko@mail.ru>
2020-10-19 10:52:03 +0300

    feat: implement meditation-app-master

---
+++ main.js
@@ -0,0 +1,61 @@
+const song = document.querySelector(".song"),
+  play = document.querySelector(".play"),
+  outline = document.querySelector(".moving-outline circle"),
+  video = document.querySelector(".vid-container video"),
+  sounds = document.querySelectorAll(".sound-picker button"),
+  timeDisplay = document.querySelector(".time-display"),
+  outlineLength = outline.getTotalLength(),
+  timeSelect = document.querySelectorAll(".time-select button");
+let fakeDuration = 600;
+
+outline.style.strokeDashoffset = outlineLength;
+outline.style.strokeDasharray = outlineLength;
+
+timeSelect.forEach((option) => {
+  option.addEventListener("click", function () {
+    fakeDuration = this.getAttribute("data-time");
+    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+      fakeDuration % 60
+    )}`;
+  });
+});
+
+const checkPlaying = (song) => {
+  if (song.paused) {
+    song.play();
+    video.play();
+    play.src = "./svg/pause.svg";
+  } else {
+    song.pause();
+    video.pause();
+    play.src = "./svg/play.svg";
+  }
+};
+
+play.addEventListener("click", () => {
+  checkPlaying(song);
+});
+
+sounds.forEach((sound) => {
+  sound.addEventListener("click", function () {
+    song.src = this.getAttribute("data-sound");
+    video.src = this.getAttribute("data-video");
+    checkPlaying(song);
+  });
+});
+
+song.ontimeupdate = () => {
+  let currentTime = song.currentTime;
+  let elapsed = fakeDuration - currentTime;
+  let seconds = Math.floor(elapsed % 60);
+  let minutes = Math.floor(elapsed / 60);
+  timeDisplay.textContent = `${minutes}:${seconds}`;
+  let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+
+  if (currentTime >= fakeDuration) {
+    song.pause();
+    song.currentTime = 0;
+    play.src = "./svg/play.svg";
+    video.pause();
+  }
+};
